bfa6e6d178cc9f67b62ff9f53eb6add3d23f60a6
laurynas <unextbusinessman@gmail.com>
2024-04-04 15:44:15 +0200

    feat: workplace relations

--- app/db/schema/workplace.ts
+++ app/db/schema/workplace.ts
@@ -1,7 +1,7 @@
 import { primaryKey, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-
 import { abstractTable } from '../abstract-table';
 import { userTable } from './user';
+import { relations, sql } from 'drizzle-orm';
 
 export const workplaceTable = abstractTable('workplace', {
   name: text('name'),
@@ -22,14 +22,49 @@ export const workplaceMemberTable = sqliteTable(
   },
   (table) => {
     return {
-      workplaceMemberPkey: primaryKey({
-        columns: [table.workplaceId, table.userId],
-        name: 'workplace_member_pkey'
-      })
+      workplaceMemberPkey: primaryKey({ columns: [table.workplaceId, table.userId], name: 'workplace_member_pkey' })
     };
   }
 );
-export const workplaceInvitationTable = abstractTable('invitation', {
+
+export const workplaceInvitationTable = sqliteTable('invitation', {
+  id: text('id')
+    .primaryKey()
+    .default(sql`(uuid4())`),
   email: text('email'),
-  workplaceId: text('workplace_id').references(() => workplaceTable.id)
+  workplaceId: text('workplace_id').references(() => workplaceTable.id),
+  createdAt: text('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull()
 });
+
+export const workplaceRelations = relations(workplaceTable, ({ one, many }) => ({
+  owner: one(userTable, {
+    fields: [workplaceTable.ownerId],
+    references: [userTable.id]
+  }),
+  workplaceMembers: many(workplaceMemberTable),
+  invitations: many(workplaceInvitationTable)
+}));
+
+export const workplaceInvitationRelation = relations(workplaceInvitationTable, ({ one }) => ({
+  user: one(userTable, {
+    fields: [workplaceInvitationTable.email],
+    references: [userTable.email]
+  }),
+  workplace: one(workplaceTable, {
+    fields: [workplaceInvitationTable.workplaceId],
+    references: [workplaceTable.id]
+  })
+}));
+
+export const workplaceMemberRelations = relations(workplaceMemberTable, ({ one }) => ({
+  workplace: one(workplaceTable, {
+    fields: [workplaceMemberTable.workplaceId],
+    references: [workplaceTable.id]
+  }),
+  user: one(userTable, {
+    fields: [workplaceMemberTable.userId],
+    references: [userTable.id]
+  })
+}));
