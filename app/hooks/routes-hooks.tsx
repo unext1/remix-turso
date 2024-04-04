58faf647591b1aed96fd67e05c943cd52e175fad
laurynas <unextbusinessman@gmail.com>
2024-04-04 15:45:52 +0200

    feat: Hook to recieve default db data, inside workplace-db

---
+++ app/hooks/routes-hooks.tsx
@@ -0,0 +1,14 @@
+import { useRouteLoaderData } from '@remix-run/react';
+import { $routeId } from 'remix-routes';
+
+import { type loader } from '~/root';
+
+import { type loader as appDasboardLayoutLoader } from '~/routes/app+/workplace+/$workplaceId+/_layout';
+
+export const useRootData = () => {
+  return useRouteLoaderData<typeof loader>('root');
+};
+
+export const useAppDashboardData = () => {
+  return useRouteLoaderData<typeof appDasboardLayoutLoader>($routeId('routes/app+/workplace+/$workplaceId+/_layout'));
+};
