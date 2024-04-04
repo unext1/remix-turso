28e4634bad765acaa9e2774053f1e9e6b289c59b
laurynas <unextbusinessman@gmail.com>
2024-04-04 16:04:16 +0200

    refactor: simplified notifciation cookie, feat: session storage cookie

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -17,5 +17,20 @@ export const sessionCookie = createTypedCookie({
 });
 
 export const notifications = createCookieSessionStorage({
-  cookie: cookie('_flash')
+  cookie: {
+    name: '_flash'
+  }
 });
+
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: '_session', // use any name you want here
+    sameSite: 'lax', // this helps with CSRF
+    path: '/', // remember to add this so the cookie will work in all routes
+    httpOnly: true, // for security reasons, make this cookie http only
+    secrets: ['s3cr3t'], // replace this with an actual secret
+    secure: process.env.NODE_ENV === 'production' // enable this in prod only
+  }
+});
+
+export const { getSession, commitSession, destroySession } = sessionStorage;
