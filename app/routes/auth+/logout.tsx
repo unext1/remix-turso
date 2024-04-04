2d595171a0cd6cddad82e8ad7a5e92e4fbf30537
laurynas <unextbusinessman@gmail.com>
2024-04-04 16:01:14 +0200

    feat(auth): logout route

---
+++ app/routes/auth+/logout.tsx
@@ -0,0 +1,8 @@
+import { redirect, type ActionFunctionArgs } from '@remix-run/node';
+import { authenticator } from '~/services/auth.server';
+
+export const loader = () => redirect('/');
+
+export const action = ({ request }: ActionFunctionArgs) => {
+  return authenticator.logout(request, { redirectTo: '/' });
+};
