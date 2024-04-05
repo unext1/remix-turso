6d66c1abc2745de563b9ea02604452e37f4dd949
laurynas <unextbusinessman@gmail.com>
2024-04-05 12:14:24 +0200

    fix: Checking if user already exists, on conflict doing nothing

--- app/routes/app+/workplace+/$workplaceId+/team.tsx
+++ app/routes/app+/workplace+/$workplaceId+/team.tsx
@@ -1,12 +1,12 @@
 import { getFormProps, getInputProps, useForm } from '@conform-to/react';
 import { getZodConstraint, parseWithZod } from '@conform-to/zod';
-import { useActionData, useLoaderData } from '@remix-run/react';
-
-import { eq } from 'drizzle-orm';
+import type { LoaderFunctionArgs, ActionFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useActionData } from '@remix-run/react';
 import { $params } from 'remix-routes';
 import { z } from 'zod';
 
-import { type ActionFunctionArgs, type LoaderFunctionArgs, json } from '@remix-run/node';
+import { useState } from 'react';
 import { CustomForm } from '~/components/custom-form';
 import { DataTable } from '~/components/data-table';
 import { columns } from '~/components/data-table/columns';
@@ -14,9 +14,10 @@ import { Button } from '~/components/ui/button';
 import { Card } from '~/components/ui/card';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
-import { H4 } from '~/components/ui/typography';
+import { H4, P } from '~/components/ui/typography';
 import { db } from '~/db';
-import { workplaceInvitationTable, workplaceMemberTable } from '~/db/schema';
+import { workplaceInvitationTable } from '~/db/schema';
+import { useAppDashboardData } from '~/hooks/routes-hooks';
 import { requireUser } from '~/services/auth.server';
 
 const schema = z.object({
@@ -24,10 +25,14 @@ const schema = z.object({
   intent: z.string().min(1, 'Intent is required')
 });
 
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+  const user = await requireUser({ request, params });
+  return json({ user });
+};
+
 export async function action({ request, params }: ActionFunctionArgs) {
   const user = await requireUser({ request, params });
   const formData = await request.formData();
-  const intent = formData.get('intent');
 
   const { workplaceId } = $params('/app/workplace/:workplaceId', params);
 
@@ -44,44 +49,30 @@ export async function action({ request, params }: ActionFunctionArgs) {
     throw Error('You are not the owner of this workplace');
   }
 
-  await db.insert(workplaceInvitationTable).values({
-    email: submission.value.email,
-    workplaceId: workplaceId
-  });
+  await db
+    .insert(workplaceInvitationTable)
+    .values({
+      email: submission.value.email,
+      workplaceId: workplaceId
+    })
+    .onConflictDoNothing();
 
   return json(submission.reply({ resetForm: true }));
 }
 
-export async function loader({ request, params }: LoaderFunctionArgs) {
-  await requireUser({ request, params });
-  const { workplaceId } = $params('/app/workplace/:workplaceId', params);
+const TeamPage = () => {
+  const dashboardData = useAppDashboardData();
 
-  const workplaceMembers = await db.query.workplaceMemberTable.findMany({
-    where: eq(workplaceMemberTable.workplaceId, workplaceId),
-    with: {
-      user: true,
-      workplace: {
-        columns: {
-          ownerId: true
-        }
-      }
-    }
-  });
-
-  const tableUsers = workplaceMembers.map((member) => ({
+  const tableUsers = dashboardData?.workplaceMembers?.map((member) => ({
     name: member.user.name,
     email: member.user.email,
     role: member.workplace.ownerId === member.userId ? 'owner' : 'user'
   }));
 
-  return json({
-    users: tableUsers
-  });
-}
-
-const TeamPage = () => {
-  const { users } = useLoaderData<typeof loader>();
   const lastResult = useActionData<typeof action>();
+
+  const [showError, setShowError] = useState<boolean>(false);
+  const usersEmails = dashboardData?.workplaceMembers?.map((workplace) => workplace.user.email);
 
   const [form, { email }] = useForm({
     lastResult,
@@ -95,27 +86,35 @@ const TeamPage = () => {
       <H4 className="tracking-wide mb-6">Team</H4>
 
       <Card className="p-6 flex-col space-y-8 flex ">
-        <DataTable data={users} columns={columns} />
+        <DataTable data={tableUsers ? tableUsers : []} columns={columns} />
       </Card>
 
-      <H4 className="tracking-wide mb-6 mt-12">Invite Member</H4>
+      {dashboardData?.isOwner ? (
+        <>
+          <H4 className="tracking-wide mb-6 mt-12">Invite Member</H4>
 
-      <Card className="p-6">
-        <CustomForm method="post" className="" {...getFormProps(form)}>
-          <div>
-            <input type="hidden" name="intent" value="inviteMember" />
+          <Card className="p-6">
+            <CustomForm method="post" className="" {...getFormProps(form)}>
+              <div>
+                <input type="hidden" name="intent" value="inviteMember" />
 
-            <Label htmlFor={email.id} className="text-xs uppercase">
-              email
-            </Label>
-            <Input {...getInputProps(email, { type: 'text' })} />
-            {email.errors && <p className="text-red-400 mt-2 uppercase text-sm">{email.errors}</p>}
-          </div>
-          <div className="flex mt-4">
-            <Button type="submit">Invite</Button>
-          </div>
-        </CustomForm>
-      </Card>
+                <Label htmlFor={email.id} className="text-xs uppercase">
+                  email
+                </Label>
+                <Input
+                  {...getInputProps(email, { type: 'text' })}
+                  onChange={(e) => setShowError(!!usersEmails?.includes(e.target.value.toLowerCase()))}
+                />
+                {showError ? <P className="text-destructive mt-2 uppercase text-sm">This user already exists</P> : null}
+                {email.errors && <p className="text-destructive mt-2 uppercase text-sm">{email.errors}</p>}
+              </div>
+              <div className="flex mt-4">
+                <Button type="submit">Invite</Button>
+              </div>
+            </CustomForm>
+          </Card>
+        </>
+      ) : null}
     </div>
   );
 };
