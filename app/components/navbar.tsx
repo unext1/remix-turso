13f5797217edf9b270f3a94e1bcee82ec15a7c0b
laurynas <unextbusinessman@gmail.com>
2024-04-05 20:20:06 +0200

    style: Change navbar styling

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,28 +1,47 @@
-import { Link } from '@remix-run/react';
+import { Link, NavLink } from '@remix-run/react';
 import { Package2 } from 'lucide-react';
 import { $path } from 'remix-routes';
 import { Button } from './ui/button';
+import { cn } from '~/utils';
 
-export const Navbar = ({ userId }: { userId?: string }) => {
+export const Navbar = ({ userId }: { userId?: string | null }) => {
   return (
-    <header className="flex items-center h-14 border-b backdrop-blur-[8px] supports-[backdrop-filter]:bg-background/80 fixed z-50 w-full px-4 lg:h-[60px] lg:px-6">
-      {/* TOP MENU */}
-      <div className="w-full grid grid-cols-3 container mx-auto items-center">
-        <div className="flex justify-start h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+    <div className="flex flex-col w-full fixed z-50 backdrop-blur-[8px] supports-[backdrop-filter]:bg-background/80 ">
+      <header className="flex h-14 items-center gap-4 border-b px-4 lg:h-[60px] lg:px-6">
+        <div className="w-full flex-1 flex items-center">
           <Link to="/" className="flex items-center gap-2 font-semibold">
             <Package2 className="h-4 w-4" />
             <span className="">Field Service</span>
           </Link>
+          <div className="pl-8 gap-6 flex">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                cn({
+                  'text-muted-foreground text-sm hover:text-primary': true,
+                  'text-white-content': isActive
+                })
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/abc"
+              className={({ isActive }) =>
+                cn({
+                  'text-muted-foreground text-sm hover:text-primary': true,
+                  'text-white-content': isActive
+                })
+              }
+            >
+              Something
+            </NavLink>
+          </div>
         </div>
-        <div className="flex justify-center">
-          <Link to="/">Home</Link>
-        </div>
-        <div className="flex justify-end">
-          <Link to={$path('/login')}>
-            <Button variant="default">{userId !== '' ? 'Go To App' : 'Login'}</Button>
-          </Link>
-        </div>
-      </div>
-    </header>
+        <Button>
+          <Link to={$path('/app')}>{userId ? 'Go To App' : 'Login'}</Link>
+        </Button>
+      </header>
+    </div>
   );
 };
