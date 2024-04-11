fec6c57cfd22ff3de553526b698e1c178bf40407
laurynas <unextbusinessman@gmail.com>
2024-04-11 12:48:10 +0200

    style: changed so you cant sort on specific columns

--- app/components/data-table/columns.tsx
+++ app/components/data-table/columns.tsx
@@ -10,32 +10,10 @@ export type Task = z.infer<typeof tableSchema>;
 
 export const columns: ColumnDef<Task>[] = [
   {
-    id: 'select',
-    header: ({ table }) => (
-      <Checkbox
-        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')}
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="translate-y-[2px]"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="translate-y-[2px]"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false
-  },
-  {
     accessorKey: 'name',
     header: ({ column }) => <DataTableColumnHeader column={column} title="Name" />,
     cell: ({ row }) => <div className="w-[80px]">{row.getValue('name')}</div>,
-    enableSorting: false,
-    enableHiding: false
+    enableSorting: true
   },
   {
     accessorKey: 'email',
@@ -65,9 +43,7 @@ export const columns: ColumnDef<Task>[] = [
         </div>
       );
     },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id));
-    }
+    enableSorting: false
   },
 
   {
