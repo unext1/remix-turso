517035daa0bd98d1aff2cc2cd822ea78f74fb310
laurynas <unextbusinessman@gmail.com>
2024-04-13 17:00:37 +0200

    feat: Exporting task assigness and timesheet tables.

--- app/db/schema-workplace/index.ts
+++ app/db/schema-workplace/index.ts
@@ -2,5 +2,7 @@ export * from './user';
 
 export * from './project';
 export * from './project-column';
-export * from './project-task';
 export * from './project-member';
+export * from './project-task';
+export * from './task-assignees';
+export * from './task-timesheet';
