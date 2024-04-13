f1cd6fb9c1cd57cea8eed92e1a9944be134f3bd0
laurynas <unextbusinessman@gmail.com>
2024-04-13 17:01:50 +0200

    feat: Relations between timesheets

--- app/db/schema-workplace/user.ts
+++ app/db/schema-workplace/user.ts
@@ -1,10 +1,12 @@
 import { relations } from 'drizzle-orm';
 import { abstractTable } from '../abstract-table';
 import { projectMemberTable, projectTable } from '.';
+import { taskTimesheetTable } from './task-timesheet';
 
 export const workplaceUser = abstractTable('user', {});
 
 export const userRelations = relations(workplaceUser, ({ many }) => ({
   memberOfProject: many(projectMemberTable),
-  ownerOfProject: many(projectTable)
+  ownerOfProject: many(projectTable),
+  taskTimesheets: many(taskTimesheetTable)
 }));
