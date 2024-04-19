3f2a89398f9c5f06e7e53a08e7b4d85351bea75c
laurynas <unextbusinessman@gmail.com>
2024-04-19 12:04:11 +0200

    refactor: Added hard-coded shadcn components

--- app/routes/app+/workplace+/$workplaceId+/index.tsx
+++ app/routes/app+/workplace+/$workplaceId+/index.tsx
@@ -1,8 +1,13 @@
-import { type LoaderFunctionArgs, json } from '@remix-run/node';
-import { useLoaderData, useLocation, useNavigation } from '@remix-run/react';
-import { $params } from 'remix-routes';
+import { json, type LoaderFunctionArgs } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+import { Activity, ArrowUpRight, CreditCard } from 'lucide-react';
 
-import { Card } from '~/components/ui/card';
+import { $params, $path } from 'remix-routes';
+import { Badge } from '~/components/ui/badge';
+import { Button } from '~/components/ui/button';
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '~/components/ui/table';
 import { H4 } from '~/components/ui/typography';
 import { requireUser } from '~/services/auth.server';
 import { getWorkplace } from '~/services/workplace.server';
@@ -29,10 +34,94 @@ const WorkplacePage = () => {
   // const fetcher = useFetcher<typeof imageAction>();
 
   return (
-    <div>
+    <div className="flex-1">
       <H4 className="tracking-wide mb-6">Dashboard</H4>
       <Card className="p-6">
-        <pre>{JSON.stringify(workplace, null, 4)}</pre>
+        <main className="flex flex-1 flex-col gap-">
+          <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Total Tasks created</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">62</div>
+                <p className="text-xs text-muted-foreground">+64% from last month</p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">+2350</div>
+                <p className="text-xs text-muted-foreground">+180.1% from last month</p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                <CreditCard className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">+12,234</div>
+                <p className="text-xs text-muted-foreground">+19% from last month</p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Active Now</CardTitle>
+                <Activity className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">+573</div>
+                <p className="text-xs text-muted-foreground">+201 since last hour</p>
+              </CardContent>
+            </Card>
+          </div>
+
+          <Card className="mt-6">
+            <CardHeader className="flex flex-row items-center">
+              <div className="grid gap-2">
+                <CardTitle>Projects</CardTitle>
+                <CardDescription>Recent projects from your workplace.</CardDescription>
+              </div>
+              <Button asChild size="sm" className="ml-auto gap-1">
+                <Link to={$path('/app/workplace/:workplaceId/projects', { workplaceId: workplace?.id || '' })}>
+                  View All
+                  <ArrowUpRight className="h-4 w-4" />
+                </Link>
+              </Button>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead className="hidden xl:table-column">Type</TableHead>
+                    <TableHead className="hidden xl:table-column">Status</TableHead>
+                    <TableHead className="hidden xl:table-column">Date</TableHead>
+                    <TableHead className="text-right">Owner</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>
+                      <div className="font-medium">Project Name</div>
+                    </TableCell>
+                    <TableCell className="hidden xl:table-column">Sale</TableCell>
+                    <TableCell className="hidden xl:table-column">
+                      <Badge className="text-xs" variant="outline">
+                        Approved
+                      </Badge>
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">2023-06-27</TableCell>
+                    <TableCell className="text-right">info@lauva.dev</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </main>
         {/* <fetcher.Form method="post" encType="multipart/form-data" action={$path('/api/images')} key={location.key}>
           <Input type="file" required name="image" accept="image/png, image/jpeg" />
           {fetcher.data ? <P className="text-red-400 mt-1">{fetcher.data.error}</P> : null}
