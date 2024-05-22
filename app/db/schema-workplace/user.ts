c756966856c4ee3de53d9ddf33aef8667a38b912
laurynas <unextbusinessman@gmail.com>
2024-05-22 21:01:15 +0200

    feat: user realtion task comment

--- app/db/schema-workplace/user.ts
+++ app/db/schema-workplace/user.ts
@@ -2,11 +2,13 @@ import { relations } from 'drizzle-orm';
 import { abstractTable } from '../abstract-table';
 import { projectMemberTable, projectTable } from '.';
 import { taskTimesheetTable } from './task-timesheet';
+import { taskCommentTable } from './task-comments';
 
 export const workplaceUser = abstractTable('user', {});
 
 export const userRelations = relations(workplaceUser, ({ many }) => ({
   memberOfProject: many(projectMemberTable),
   ownerOfProject: many(projectTable),
-  taskTimesheets: many(taskTimesheetTable)
+  taskTimesheets: many(taskTimesheetTable),
+  taskComments: many(taskCommentTable)
 }));
