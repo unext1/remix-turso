2ee99685ddadf87c41f39d9d857d738e357a17aa
laurynas <unextbusinessman@gmail.com>
2024-05-23 04:43:48 +0200

    feat: Changed default columns, refactored page

--- app/routes/app+/workplace+/$workplaceId+/projects+/index.tsx
+++ app/routes/app+/workplace+/$workplaceId+/projects+/index.tsx
@@ -2,7 +2,7 @@ import { getFormProps, getInputProps, useForm } from '@conform-to/react';
 import { getZodConstraint, parseWithZod } from '@conform-to/zod';
 import { json, redirect, type ActionFunctionArgs, type LoaderFunctionArgs } from '@remix-run/node';
 
-import { useActionData, useFetcher, useLoaderData, useNavigation } from '@remix-run/react';
+import { useActionData, useLoaderData, useNavigation } from '@remix-run/react';
 import { $params, $path } from 'remix-routes';
 import { z } from 'zod';
 
@@ -26,7 +26,7 @@ import { workplaceDb } from '~/db';
 import { projectColumnTable, projectMemberTable, projectTable } from '~/db/schema-workplace';
 
 import { useAppDashboardData } from '~/hooks/routes-hooks';
-import { requireUser } from '~/services/auth.server';
+import { type SessionUser, requireUser } from '~/services/auth.server';
 import { getAllMyProjects } from '~/services/project.server';
 
 const schema = z.object({
@@ -67,18 +67,23 @@ export async function action({ request, params }: ActionFunctionArgs) {
       .insert(projectColumnTable)
       .values([
         {
-          name: '💡 To Do',
+          name: '❌ Not Started',
           order: 1,
           projectId: newProject[0].id
         },
         {
-          name: '⏳ In Progress',
+          name: '💡 To Do',
           order: 2,
           projectId: newProject[0].id
         },
         {
-          name: '💰 Done',
+          name: '⏳ In Progress',
           order: 3,
+          projectId: newProject[0].id
+        },
+        {
+          name: '💸 Done',
+          order: 4,
           projectId: newProject[0].id
         }
       ]);
@@ -104,14 +109,55 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
 const ProjectPage = () => {
   const { user, projects, workplaceId } = useLoaderData<typeof loader>();
-  const lastResult = useActionData<typeof action>();
   const workplaceData = useAppDashboardData();
 
-  const projectOwner = workplaceData?.workplaceMembers?.find((i) => i.workplace.ownerId)?.user;
+  return (
+    <div className="h-full flex flex-col">
+      <div className="flex justify-between">
+        <H4 className="tracking-wide mb-6">Projects</H4>
+        <CreateProjectDialog user={user} />
+      </div>
 
-  const fetcher = useFetcher();
+      {projects.length >= 1 ? (
+        <div className="grid sm:grid-cols-2 grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {projects.map((project) => {
+            const projectOwner = workplaceData?.workplaceMembers?.find(
+              (member) => member.userId === project.ownerId
+            )?.user;
+            return (
+              <div key={project.id}>
+                <CustomCard
+                  ownerId={project.ownerId || ''}
+                  userId={user.id}
+                  userEmail={projectOwner?.email || ''}
+                  workplaceId={workplaceId}
+                  name={project.name}
+                  projectId={project.id}
+                />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex flex-1 items-center justify-center rounded-lg border shadow-sm w-full">
+          <div className="flex flex-col items-center gap-1 text-center">
+            <h3 className="text-2xl font-bold tracking-tight">You have no Projects</h3>
+            <p className="text-sm text-muted-foreground mb-4">
+              You can start as soon as you create your first Project.
+            </p>
+            <CreateProjectDialog user={user} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProjectPage;
+
+const CreateProjectDialog = ({ user }: { user: SessionUser }) => {
+  const lastResult = useActionData<typeof action>();
   const navigation = useNavigation();
-  const isCreating = navigation.state;
 
   const [form, { name, ownerId }] = useForm({
     lastResult,
@@ -121,131 +167,45 @@ const ProjectPage = () => {
   });
 
   return (
-    <div className="flex-1">
-      <div className="flex justify-between">
-        <H4 className="tracking-wide mb-6">Projects</H4>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="default" size="sm">
-              Create Project
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Create Project</DialogTitle>
-              <DialogDescription>
-                Make changes to your profile here. Click save when you&apos;re done.
-              </DialogDescription>
-            </DialogHeader>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="default" size="sm">
+          Create Project
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Project</DialogTitle>
+          <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
+        </DialogHeader>
 
-            <CustomForm method="post" className="grid gap-4 py-4" {...getFormProps(form)}>
-              <div className="grid grid-cols-6 items-center gap-x-4 gap-y-2">
-                <Input {...getInputProps(ownerId, { type: 'number' })} type="hidden" value={user.id} />
-                <Label htmlFor={name.id} className="text-right col-span-2 text-sm whitespace-nowrap w-fit">
-                  Project Name
-                </Label>
+        <CustomForm method="post" className="grid gap-4 py-4" {...getFormProps(form)}>
+          <div className="grid grid-cols-6 items-center gap-x-4 gap-y-2">
+            <Input {...getInputProps(ownerId, { type: 'number' })} type="hidden" value={user.id} />
+            <Label htmlFor={name.id} className="text-right col-span-2 text-sm whitespace-nowrap w-fit">
+              Project Name
+            </Label>
 
-                <Input
-                  {...getInputProps(name, { type: 'text' })}
-                  placeholder="My awesome project"
-                  className="col-span-4"
-                />
-                {name.errors && <p className="text-red-400 mt-2 uppercase text-sm">{name.errors}</p>}
+            <Input {...getInputProps(name, { type: 'text' })} placeholder="My awesome project" className="col-span-4" />
+            {name.errors && <p className="text-red-400 mt-2 uppercase text-sm">{name.errors}</p>}
 
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Label htmlFor="defaultColumns" className="text-right col-span-2 text-sm whitespace-nowrap w-fit">
-                        Default Columns
-                      </Label>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <p>Default columns: Todo, In progress, Done</p>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-                <Checkbox name="defaultColumns" />
-              </div>
-              <Button type="submit">{navigation.location ? 'Creating...' : 'Create Project'}</Button>
-            </CustomForm>
-          </DialogContent>
-        </Dialog>
-      </div>
-
-      {projects.length >= 1 ? (
-        projects.map((project) => (
-          <div className="grid sm:grid-cols-2 grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4" key={project.id}>
-            <CustomCard
-              ownerId={project.ownerId || ''}
-              userId={user.id}
-              userEmail={projectOwner?.email || ''}
-              workplaceId={workplaceId}
-              name={project.name}
-              projectId={project.id}
-            />{' '}
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Label htmlFor="defaultColumns" className="text-right col-span-2 text-sm whitespace-nowrap w-fit">
+                    Default Columns
+                  </Label>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Default columns: Not Started, Todo, In Progress, Done</p>{' '}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <Checkbox name="defaultColumns" />
           </div>
-        ))
-      ) : (
-        <div className="flex flex-1 items-center justify-center rounded-lg border shadow-sm w-full h-full">
-          <div className="flex flex-col items-center gap-1 text-center">
-            <h3 className="text-2xl font-bold tracking-tight">You have no Projects</h3>
-            <p className="text-sm text-muted-foreground mb-4">
-              You can start as soon as you create your first Project.
-            </p>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button variant="default" size="sm" className="px-6">
-                  Create Project
-                </Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                  <DialogTitle>Create Project</DialogTitle>
-                  <DialogDescription>
-                    Make changes to your profile here. Click save when you&apos;re done.
-                  </DialogDescription>
-                </DialogHeader>
-
-                <CustomForm method="post" className="grid gap-4 py-4" {...getFormProps(form)}>
-                  <div className="grid grid-cols-6 items-center gap-x-4 gap-y-2">
-                    <Input {...getInputProps(ownerId, { type: 'number' })} type="hidden" value={user.id} />
-                    <Label htmlFor={name.id} className="text-right col-span-2 text-sm whitespace-nowrap w-fit">
-                      Project Name
-                    </Label>
-
-                    <Input
-                      {...getInputProps(name, { type: 'text' })}
-                      placeholder="My awesome project"
-                      className="col-span-4"
-                    />
-                    {name.errors && <p className="text-red-400 mt-2 uppercase text-sm">{name.errors}</p>}
-
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <Label
-                            htmlFor="defaultColumns"
-                            className="text-right col-span-2 text-sm whitespace-nowrap w-fit"
-                          >
-                            Default Columns
-                          </Label>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Default columns: Todo, In progress, Done</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                    <Checkbox name="defaultColumns" />
-                  </div>
-                  <Button type="submit">{navigation.location ? 'Creating...' : 'Create Project'}</Button>
-                </CustomForm>
-              </DialogContent>
-            </Dialog>
-          </div>
-        </div>
-      )}
-    </div>
+          <Button type="submit">{navigation.location ? 'Creating...' : 'Create Project'}</Button>
+        </CustomForm>
+      </DialogContent>
+    </Dialog>
   );
 };
-
-export default ProjectPage;
